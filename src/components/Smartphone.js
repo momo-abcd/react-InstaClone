8685a2048e37d03ba7fc576a748989130c78ce3e
yang <yang@naver.com>
2020-12-17 09:07:39 +0900

    makeSlider_1

--- src/components/Smartphone.js
+++ src/components/Smartphone.js
@@ -4,17 +4,17 @@ import './Smartphone.css';
 
 function Smartphone(props){
 
-    console.log(document.querySelector('#first'));
         return (
             <div>
         <div className="container">
         <div><img src="/images/smartphone.PNG" draggable="false" alt="smartphone"></img></div>
             <div className="changeImg">
-                <img id="first" className = "basicImg nowImg" src="/images/screenshot01.jpg" alt=""></img>
+                {/* <img id="first" className = "basicImg nowImg" src="/images/screenshot01.jpg" alt=""></img>
                 <img id="second" className = "basicImg nowImg nextImg" src="/images/screenshot02.jpg" alt=""></img>
                 <img id="third" className = "basicImg" src="/images/screenshot03.jpg" alt=""></img>
                 <img id="fourth" className = "basicImg" src="/images/screenshot04.jpg" alt=""></img>
-                <img id="fifth" className = "basicImg" src="/images/screenshot05.jpg" alt=""></img>
+                <img id="fifth" className = "basicImg" src="/images/screenshot05.jpg" alt=""></img> */}
+                <BackgroundSlider />
             </div>
         </div>
         </div>
