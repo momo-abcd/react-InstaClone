e48cd13d3762e4d2a4a8c09007bed2234fe9aa89
momo-abcd <yym4481@naver.com>
2021-01-07 22:30:04 +0900

    'third'

--- src/components/AuthRoute.js
+++ src/components/AuthRoute.js
@@ -2,10 +2,11 @@ import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 import { Route } from 'react-router-dom';
 import Smarthphone from '../components/Smartphone';
+import MainLogin from '../components/MainLogin';
 
 import { connect } from 'react-redux';
 
-function AuthRoute({ auth ,...rest}) {
+function AuthRoute({ auth }) {
 
         // const [isAuth,setIsAuth] = useState(isAuthenticated);
         const [isLoding, setIsLoding] = useState(true);
@@ -23,14 +24,14 @@ function AuthRoute({ auth ,...rest}) {
         // </Route>
         <Route>
             {/* <div>{auth ? <p>{auth}auth</p> : '456'}</div> */}
-            <div>{auth}</div>
+            <div>{auth.isAuthenticated ? 'login 성공' : <MainLogin />}</div>
         </Route>
     );
 }
 
 // export default AuthRoute;
 const mapStateToProps = (state) => ({
-    auth:state
+    auth:state.auth
 });
 
 export default connect(mapStateToProps, null)(AuthRoute);
