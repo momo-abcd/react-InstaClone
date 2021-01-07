13de3e4ed8b05eb3d415aee3b2d54b5ca470c218
momo-abcd <yym4481@naver.com>
2021-01-07 22:25:41 +0900

    'seconde'

--- src/components/AuthRoute.js
+++ src/components/AuthRoute.js
@@ -3,23 +3,34 @@ import axios from 'axios';
 import { Route } from 'react-router-dom';
 import Smarthphone from '../components/Smartphone';
 
-function AuthRoute({ isAuthenticated, component: Component, render, ...rest }) {
+import { connect } from 'react-redux';
 
-        const [isAuth,setIsAuth] = useState(isAuthenticated);
+function AuthRoute({ auth ,...rest}) {
+
+        // const [isAuth,setIsAuth] = useState(isAuthenticated);
         const [isLoding, setIsLoding] = useState(true);
         // console.log(isLoding);
         useEffect(() => {
-            console.log(rest);
+            // console.log(rest);
             setIsLoding(false);
             return setIsLoding(false);
         },[])
-        
+        console.log(auth);
     return (
+        // <Route>
+        //     {isAuthenticated ? <Smarthphone /> : 
+        //     (isLoding ? <img src='/images/loding.jpg'></img> : Component)}
+        // </Route>
         <Route>
-            {isAuthenticated ? <Smarthphone /> : 
-            (isLoding ? <img src='/images/loding.jpg'></img> : Component)}
+            {/* <div>{auth ? <p>{auth}auth</p> : '456'}</div> */}
+            <div>{auth}</div>
         </Route>
     );
 }
 
-export default AuthRoute;
+// export default AuthRoute;
+const mapStateToProps = (state) => ({
+    auth:state
+});
+
+export default connect(mapStateToProps, null)(AuthRoute);
